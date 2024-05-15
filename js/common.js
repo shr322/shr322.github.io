48e3faa7d9e45bf58be56297d2087351221a0daa
Hyung Rae Sim <58067301+shr322@users.noreply.github.com>
2024-05-15 23:00:32 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,132 +1,8 @@
 window.addEventListener("load", function () {
   document.querySelector(".main").style.height = window.innerHeight + "px";
 
-  setTimeout(() => {
-    document.body.classList.add("on");
-  }, 2000);
-
   document.querySelector('.user_name').innerHTML = paramValue;
 });
-
-const wrap = document.querySelector("#wrap");
-const btns = wrap.querySelectorAll("#nav li ");
-const boxs = wrap.querySelectorAll(".cont");
-const activeClass = "on";
-
-let posArr = null;
-let enableClick = true;
-
-// 버튼 on index 값
-function activeIndex() {
-  for (let i = 0; i < btns.length; i++) {
-    if (btns[i].classList.contains(activeClass)) {
-      return i;
-    }
-  }
-}
-
-// 동작 함수
-function bindingEvent() {
-  setPos();
-
-  // 버튼 클릭 시
-  btns.forEach(function (el, i) {
-    el.addEventListener("click", function () {
-      const isOn = el.classList.contains(activeClass);
-
-      if (enableClick && !isOn) {
-        enableClick = false;
-        moveScroll(i);
-      } else {
-        moveScroll(i);
-      }
-    });
-  });
-
-  // 브라우져 스크롤 시
-  window.addEventListener("scroll", function (e) {
-    e.preventDefault();
-
-    // 구름
-    // document.querySelector(".cloud1").style.left = `${
-    //   (50 + window.scrollY) / 5
-    // }px`;
-    // document.querySelector(".cloud2").style.right = `${window.scrollY / 2}px`;
-    // document.querySelector(".cloud3").style.left = `${
-    //   (1000 + window.scrollY) / 10
-    // }px`;
-
-    let scroll = window.scrollY;
-    activation(scroll);
-  });
-}
-
-// posArr 배열에 각 section에 offsetTop 값 구하기.
-function setPos() {
-  posArr = [];
-  boxs.forEach(function (el, i) {
-    posArr.push(el.offsetTop);
-  });
-}
-
-// 윈도우 스크롤 시 저장해서 마우스 휠 할때 실행
-function moveScroll(i) {
-  window.scroll({
-    behavior: "smooth",
-    top: posArr[i],
-  });
-  enableClick = true;
-}
-
-// 브라우져 스크롤 시 posArr에 담긴 값이랑 브라우져 높이 값이랑 비교해서 버튼에 클래스 수정
-function activation(scroll) {
-  for (let i = 0; i < btns.length; i++) {
-    if (scroll >= posArr[i] - 200) {
-      for (let j = 0; j < btns.length; j++) {
-        btns[j].classList.remove(activeClass);
-      }
-      btns[i].classList.add(activeClass);
-    }
-  }
-}
-
-bindingEvent();
-
-/* 이미지 */
-const swiper = new Swiper(".swiper", {
-  loop: false,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
-// function setScreenSize() {
-//   let vh = window.innerHeight * 0.01;
-
-//   document.documentElement.style.setProperty("--vh", `${vh}px`);
-// }
-
-// setScreenSize();
-
-/* 상단 타이핑 */
-// const content = "심형래❤김수지";
-// const text = document.querySelector(".text");
-// let i = 0;
-
-// function typing() {
-//   if (i < content.length) {
-//     let txt = content.charAt(i);
-//     text.innerHTML += txt;
-//     i++;
-//   }
-// }
-
-// setInterval(typing, 200);
 
 /* naver map */
 var elluce = new naver.maps.LatLng(37.5222098, 127.038892);
@@ -236,43 +112,6 @@ function showToast(message) {
   }, 3000);
 }
 
-// 스크롤 이벤트를 차단하는 함수
-function preventScroll(event) {
-  event.preventDefault();
-}
-
-// 스크롤 이벤트 리스너 추가하여 스크롤 차단
-function disableScroll() {
-  document.body.addEventListener('touchmove', preventScroll, { passive: false });
-  document.body.addEventListener('mousewheel', preventScroll, { passive: false });
-}
-
-// 스크롤 이벤트 리스너 제거하여 스크롤 활성화
-function enableScroll() {
-  document.body.removeEventListener('touchmove', preventScroll);
-  document.body.removeEventListener('mousewheel', preventScroll);
-}
-
-
-$(".galley li").on("click", function () {
-  let index = $(this).index();
-  console.log(index);
-  $(".popup").show();
-  swiper.slideTo(index);
-  $("body").addClass("pop");
-
-  disableScroll()
-
-});
-
-$(".close").on("click", function () {
-  $(".popup").hide();
-  $("body").removeClass("pop");
-
-  enableScroll()
-});
-
-
 // 현재 URL에서 파라미터 문자열을 가져옵니다.
 const queryString = window.location.search;
 
@@ -284,4 +123,14 @@ const paramValue = urlParams.get('name');
 
 
 
-
+// 스크롤시 스르륵
+window.addEventListener('scroll', function() {
+  const elements = document.querySelectorAll('.scroll-element');
+  elements.forEach(element => {
+    if (element.getBoundingClientRect().top < window.innerHeight) {
+      setTimeout(() => {
+        element.classList.add('visible');
+      }, 100);
+    }
+  });
+});
